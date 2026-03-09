97ecb09e4265027a0df36ba8c2c57d9e1164247f
SATHYAGITH368 <142714885+SATHYAGITH368@users.noreply.github.com>
2026-03-09 23:50:26 +0530

    Add files via upload

---
+++ software/waypoint navigation-simulation/KD_1002_waypoint_service.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Pose
+from waypoint_navigation.srv import GetWaypoints
+
+class WayPoints(Node):
+
+    def __init__(self):
+        super().__init__('waypoints_service')
+        self.srv = self.create_service(GetWaypoints, 'waypoints', self.waypoint_callback)
+
+        # Predefined waypoints
+        self.waypoints = [
+     [-7.0, 0.0, 29.22],
+     [-7.64, 3.06, 29.22],
+     [-8.22, 6.02, 29.22],
+ [-9.11, 9.27, 29.27],
+ [-5.98, 8.81, 29.27],
+ [-3.26, 8.41, 29.88],
+ [0.87, 8.18, 29.05],
+ [3.93, 7.35, 29.05] ,
+     
+   
+]
+
+    def waypoint_callback(self, request, response):
+        if request.get_waypoints:
+            response.waypoints.poses = [Pose() for _ in range(len(self.waypoints))]
+            for i in range(len(self.waypoints)):
+                response.waypoints.poses[i].position.x = self.waypoints[i][0]
+                response.waypoints.poses[i].position.y = self.waypoints[i][1]
+                response.waypoints.poses[i].position.z = self.waypoints[i][2]
+            self.get_logger().info("Incoming request for Waypoints")
+            return response
+        else:
+            self.get_logger().info("Request rejected")
+            return response  # always return a response
+
+def main():
+    rclpy.init()
+    waypoints = WayPoints()
+
+    try:
+        rclpy.spin(waypoints)
+    except KeyboardInterrupt:
+        waypoints.get_logger().info('KeyboardInterrupt, shutting down.\n')
+    finally:
+        waypoints.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+        
